2998b601d1f2da8e4b0e248a47ebd88ab09f1a74
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2022-12-19 12:48:45 -0500

    changed heading

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <>
       <div className="headingDiv">
-        <h1 className="heading">Brandon's Resume</h1>
+        <h1 className="heading">The PERN Pit</h1>
       </div>
       <div>
         <Navbar />
