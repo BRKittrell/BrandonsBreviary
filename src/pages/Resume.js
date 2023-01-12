e21e5593452ca95dd5b78cbfb03dce2442719c2a
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2023-01-11 22:21:13 -0500

    added new resume href

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ import React from "react";
 const Resume = () => {
   return (
 <div className="resumeContainer">
-    <a href="https://docs.google.com/document/d/e/2PACX-1vQBtweF02PahfNiX-7jMwZp74KKjZXIEM3NMTw5_YLGznfW5sSE34eZerXPnSCPig/pub?embedded=true" download='GFG'>
+    <a href="https://drive.google.com/drive/folders/1JlqQKMyZ6P_a26SnnEUravvnM8tLUP0I" download='GFG'>
     {/* <button type="button">Download</button> */}
       </a>
       <iframe
