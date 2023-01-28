56cdfaec4416fb562613f940eb9eaa6ebf1216a8
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2023-01-27 19:42:11 -0500

    server routes added

--- server/server.js
+++ server/server.js
@@ -28,6 +28,57 @@ app.post('/visitors', async (req,res) =>{
     }
 })
 
+app.get('/visitors', async (res) => {
+    try {
+        const {rows} = await pool.query('SELECT * FROM visitors');
+        res.send(rows);
+    } catch (err){
+        res.send(err.message);
+    }
+})
+
+app.get('/visitors/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const {rows} = await pool.query('SELECT * FROM the_pets WHERE pet_id = $1', [id])
+        res.send(rows)  
+    }catch (error){
+        console.error(error);
+    }
+})
+
+app.post('/pets', async (req, res) => {
+    try{
+        const {name, age, kind} = req.body
+        const {rows} = await pool.query('INSERT INTO the_pets(name, age, kind) VALUES($1, $2, $3)', [name, age, kind])
+        res.send(rows) 
+    }catch (error){
+        res.send(error);
+}
+})
+
+app.patch('/pets/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const {name, age, kind} = req.body
+        const {rows} = await pool.query('UPDATE the_pets SET name = $1, age = $2, kind = $3 WHERE pet_id = $4', [name, age, kind, id])
+        res.send(rows);
+    }catch (error){ 
+        res.send(error);
+}
+})
+
+app.delete('/pets/:id', async (req, res) => {
+    const {id} = req.params
+    try{
+        const {rows} = await pool.query('DELETE FROM the_pets WHERE pet_id = $1', [id])
+        res.send(rows)
+    }catch{
+        res.send(error);
+    }
+
+})
+
 app.listen(process.env.PORT, () =>{
     console.log('Listening port:' + PORT)
 })
