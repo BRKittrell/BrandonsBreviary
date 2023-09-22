c6cefc311e6d3ee412d23b3a589c8d962ff5a84d
Brandon Kittrell <brandonkittrell@Brandons-MBP.lan1>
2023-09-22 08:52:04 -0400

    wtf

---
+++ build/precache-manifest.ed1f1a4abf2f20f6849fe1232529f2cb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd04261cd0ffd84241d2",
+    "url": "/static/css/main.94a7d313.chunk.css"
+  },
+  {
+    "revision": "cd04261cd0ffd84241d2",
+    "url": "/static/js/main.967ea4cf.chunk.js"
+  },
+  {
+    "revision": "234ecdafbb2600fe3a8a",
+    "url": "/static/js/runtime~main.35de78a9.js"
+  },
+  {
+    "revision": "78ff620bd15faf207878",
+    "url": "/static/js/2.f17f9021.chunk.js"
+  },
+  {
+    "revision": "c702756c9cb0ce7eb52f",
+    "url": "/static/js/3.d8ac33f5.chunk.js"
+  },
+  {
+    "revision": "35ac6a61edc754e1570e58a05c7daed5",
+    "url": "/static/media/corn.35ac6a61.jpeg"
+  },
+  {
+    "revision": "089c0d670f4a1aa96443a25dca761fac",
+    "url": "/static/media/LI_Pic.089c0d67.jpeg"
+  },
+  {
+    "revision": "704c8b6f10ccf78e6f5c9c4c39c8caac",
+    "url": "/static/media/AlaskaFish.704c8b6f.jpeg"
+  },
+  {
+    "revision": "6ba6a63a1c5752d94af18d3b2a3cecaa",
+    "url": "/static/media/OIF.6ba6a63a.jpeg"
+  },
+  {
+    "revision": "fd718911ebd2c70f49ccb1822927803a",
+    "url": "/static/media/CSEL.fd718911.jpeg"
+  },
+  {
+    "revision": "e56ad5f15ef340d2622a12306dd750af",
+    "url": "/static/media/PSUMSUGame.e56ad5f1.jpeg"
+  },
+  {
+    "revision": "789a89f6ec9f4f70e247986656017664",
+    "url": "/static/media/RickieFowler.789a89f6.jpeg"
+  },
+  {
+    "revision": "52ce3db5f5b5369cda692a3508b64be9",
+    "url": "/static/media/Roo.52ce3db5.jpeg"
+  },
+  {
+    "revision": "477a7c841fca214283cf2324492e31ef",
+    "url": "/index.html"
+  }
+];
